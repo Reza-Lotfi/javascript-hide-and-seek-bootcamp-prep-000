005662e42f4c562a6ec11c5307e33e2188136200
Reza Lotfi <rezaiscool7@aol.com>
2018-09-25 02:19:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested div div div div')
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('ul.ranked-list li');
+  lis.forEach(function(li){
+    li.innerHTML = (parseInt(li.innerHTML) + n)
+  })
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
+}
